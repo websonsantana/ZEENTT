c2e702b78191bfa870f6e8cf64b7174d5be9abf2
WEBSON JOSE ALBUQUERQUE DE SANTANA <159049666+websonsantana@users.noreply.github.com>
2025-04-10 23:53:24 -0300

    Atualizar o menu.js

--- menu.js
+++ menu.js
@@ -1,14 +1,13 @@
 function selecionarItem(item) {
-    // Remove a classe 'selected' de todos os itens
-    const itens = document.querySelectorAll('.item');
-    itens.forEach(i => i.classList.remove('selected'));
-  
-    // Adiciona a classe 'selected' ao item clicado
-    item.classList.add('selected');
-  }
-  <script>
-  function toggleMenu() {
-    const menu = document.querySelector('.mobile-menu');
-   }
+  // Remove a classe 'selected' de todos os itens
+  const itens = document.querySelectorAll('.item');
+  itens.forEach(i => i.classList.remove('selected'));
 
-</script>
+  // Adiciona a classe 'selected' ao item clicado
+  item.classList.add('selected');
+}
+
+function toggleMenu() {
+  const menu = document.querySelector('.mobile-menu');
+  menu.classList.toggle('active'); // Alterna a classe 'active'
+}
