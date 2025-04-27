347e4141ee1f9d4a4a841050039d90d037006b01
WEBSON JOSE ALBUQUERQUE DE SANTANA <159049666+websonsantana@users.noreply.github.com>
2025-04-27 15:51:21 -0300

    Add files via upload

---
+++ PAGINA-VENDAS-ZEENTT/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('scroll', function() {
+    const elements = document.querySelectorAll('.aparecer');
+    elements.forEach(el => {
+      const rect = el.getBoundingClientRect();
+      if (rect.top < window.innerHeight - 50) {
+        el.classList.add('mostrar');
+      }
+    });
+  });
+  const texto = "Precisou, fala com a ZEENTT, tudo para seu restaurante em um só lugar!";
+  const elemento = document.getElementById('texto-digitando');
+  let index = 0;
+  
+  function digitar() {
+    if (index < texto.length) {
+      elemento.innerHTML += texto.charAt(index);
+      index++;
+      setTimeout(digitar, 50);
+    }
+  }
+  digitar();
+    const menuToggle = document.querySelector('.menu-toggle');    
